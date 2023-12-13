8ac7ad117e9f6566489ee7f3798bc7411ab0f921
moin raza <117511472+moinraza123@users.noreply.github.com>
2023-12-13 12:44:39 +0530

    Add files via upload

---
+++ predefine function.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#predefine functions
+
+
+# In[2]:
+
+
+#1 String Functions:
+
+
+# In[3]:
+
+
+string=input("enter string : ")
+
+
+# In[4]:
+
+
+len(string)
+
+
+# In[5]:
+
+
+string1=string.lower()
+
+
+# In[6]:
+
+
+string1
+
+
+# In[7]:
+
+
+string2=string.upper()
+
+
+# In[8]:
+
+
+string2
+
+
+# In[9]:
+
+
+#2 list fuction
+
+
+# In[10]:
+
+
+num1 = [1,23,4,5,6,7,8,9]
+
+
+# In[11]:
+
+
+num1
+
+
+# In[15]:
+
+
+max(num1)
+
+
+# In[16]:
+
+
+min(num1)
+
+
+# In[17]:
+
+
+num2=sum(num1)
+
+
+# In[18]:
+
+
+num2
+
+
+# In[19]:
+
+
+#3Input and Conversion Functions:
+
+
+# In[20]:
+
+
+num = input("enter number")
+
+
+# In[21]:
+
+
+num2 = int(num)
+
+
+# In[22]:
+
+
+num2
+
+
+# In[23]:
+
+
+type(num2)
+
+
+# In[24]:
+
+
+type(num)
+
+
+# In[25]:
+
+
+num1 = input("enter number")
+
+
+# In[26]:
+
+
+num3=float(num1)
+
+
+# In[27]:
+
+
+num3
+
+
+# In[28]:
+
+
+type(num3)
+
+
+# In[29]:
+
+
+type(num1)
+
+
+# In[30]:
+
+
+#4 Math Functions:
+
+
+# In[31]:
+
+
+number = int(input("enter number"))
+
+
+# In[32]:
+
+
+abs(number) #find absulate value of number
+
+
+# In[33]:
+
+
+poinnum= float(input("enter number"))
+
+
+# In[34]:
+
+
+round(poinnum)
+
+
+# In[35]:
+
+
+#5 List Manipulation:
+
+
+# In[36]:
+
+
+list1 = ["apple", "banana", "cherry"]
+
+
+# In[37]:
+
+
+list2 = [1, 2, 3]
+
+
+# In[38]:
+
+
+combined_pairs = list(zip(list1, list2))
+
+
+# In[39]:
+
+
+combined_pairs
+
+
+# In[40]:
+
+
+fruits = ["orange", "apple", "banana", "grape", "cherry"]
+
+
+# In[41]:
+
+
+sorted_fruits = sorted(fruits)
+
+
+# In[42]:
+
+
+print(sorted_fruits)
+
+
+# In[43]:
+
+
+num12=list(range(2,20,2))
+
+
+# In[44]:
+
+
+num12
+
+
+# In[ ]:
+
+
+
+
